9e05a9127645474eaa88246fd930f09a3ffa2bb5
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-01-10 10:43:17 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
