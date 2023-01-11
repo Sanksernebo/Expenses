0cd21ce99a2b243e8395e37f6fb6a1040310942f
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-01-11 15:07:02 +0200

    Ülesanne 5 new patch

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,7 +1,6 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 function Expenses(props){
-    console.log(props)
     return (
     <div>
         <ExpenseItem expenseData={props.expenseData[0]}></ExpenseItem>
