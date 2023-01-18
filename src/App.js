0d68055249420e9be20811046c95534b22872007
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-01-18 14:21:52 +0200

    Ülesanne 7 esimene

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
-import ExpenseItem from "./components/Expenses/ExpenseItem";
 
-function App() {
+const App = () => {
     const expenses = [
         {
             date: new Date(2023, 0, 10),
